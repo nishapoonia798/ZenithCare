66d35d7e90da6ac7b198549ca427ea255099af26
Thorn_Bird <nishapoonia798@gmail.com>
2025-04-07 22:35:07 +0530

    header renamed and footer added

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1rem', background: '#eee' }}>
+      <p>© 2025 ZenithCare. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
